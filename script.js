3e3355999c917a63806ef26f504d6df739da08c2
Tine56 <justinereyes747@gmail.com>
2024-12-07 21:03:56 +0800

    itc

---
+++ script.js
@@ -0,0 +1,5 @@
+function revealText(item) {
+    // Toggle the "active" class to show or hide the description
+    const description = item.querySelector('.description');
+    item.classList.toggle('active');
+}
